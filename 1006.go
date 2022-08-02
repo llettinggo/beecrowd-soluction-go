e5c1e9c6516aa0721948685bbb65ef65a92d526d
David Alves <davidalves101@gmail.com>
2022-08-02 10:42:08 -0300

    1006

---
+++ 1006.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	var noteA, noteB, noteC, result float32
+
+	fmt.Scan(&noteA)
+	fmt.Scan(&noteB)
+	fmt.Scan(&noteC)
+
+	result = (noteA*2 + noteB*3 + noteC*5) / (2 + 3 + 5)
+
+	fmt.Printf("MEDIA = %.1f\n", result)
+
+}
